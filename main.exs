c39a99b2c0299e21f359195b11736c31c581c76b
Matt McCoy <matt@inspectiongo.com>
2024-04-03 23:41:48 -0400

    Cleanup single file example

--- main.exs
+++ main.exs
@@ -46,27 +46,15 @@ defmodule Example.PanelLive do
 
   def render(assigns) do
     ~H"""
-    <div class="w-screen">
-      <div class="border-l flex flex-row">
-        <div class="flex flex-col min-w-14 items-center">
-          <.tab_button
-            tab={:messages_tab}
-            icon="hero-chat-bubble-bottom-center-text"
-            text="Messages tab"
-            live_action={@live_action}
-            route={~p"/messages_tab"}
-          />
-          <.tab_button
-            tab={:settings_tab}
-            icon="hero-cog-6-tooth"
-            text="Settings tab"
-            live_action={@live_action}
-            route={~p"/settings_tab"}
-          />
+    <div>
+      <div>
+        <div>
+          <.tab_button text="Messages tab" route={~p"/messages_tab"} />
+          <.tab_button text="Settings tab" route={~p"/settings_tab"} />
         </div>
 
-        <aside class="border border-black !w-[270px]">
-          <div class="flex flex-col">
+        <aside>
+          <div>
             <div :if={@live_action == :messages_tab}>
               <.live_component module={Example.MessagesTab} id="messages_tab" />
             </div>
@@ -84,13 +72,8 @@ defmodule Example.PanelLive do
 
   defp tab_button(assigns) do
     ~H"""
-    <button
-      type="button"
-      class={["mb-4 mt-2", if(@live_action == @tab, do: "bg-gray-200")]}
-      phx-click={JS.patch(@route)}
-    >
+    <button type="button" phx-click={JS.patch(@route)}>
       <%= @text %>
-      <%!-- <.icon name={@icon} /> --%>
     </button>
     """
   end
@@ -119,7 +102,7 @@ defmodule Example.MessagesTab do
 
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col">
+    <div>
       <.live_component
         module={Example.MessageComponent}
         id="some_unique_message_id"
@@ -127,12 +110,11 @@ defmodule Example.MessagesTab do
       />
       <form
         id="full_add_message_form"
-        class="flex flex-col"
         phx-change="add_message_change"
         phx-submit="add_message"
         phx-target="#full_add_message_form"
       >
-        <.input id="new_message_input" name="new_message" class="mt-2" value={@value} />
+        <.input id="new_message_input" name="new_message" value={@value} />
       </form>
     </div>
     """
@@ -145,11 +127,6 @@ defmodule Example.MessagesTab do
         name={@name}
         id={@id}
         value={Phoenix.HTML.Form.normalize_value("text", @value)}
-        class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          "phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
-          "border-zinc-300 focus:border-zinc-400",
-        ]}
       />
     </div>
     """
