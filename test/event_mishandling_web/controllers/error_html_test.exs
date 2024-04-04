4bcb9f03ce42a75f57e3bef6257896690e7d61db
Matt McCoy <matt@inspectiongo.com>
2024-04-03 20:57:15 -0400

    Cleanup

--- test/event_mishandling_web/controllers/error_html_test.exs
+++ test/event_mishandling_web/controllers/error_html_test.exs
@@ -9,7 +9,6 @@ defmodule EventMishandlingWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(EventMishandlingWeb.ErrorHTML, "500", "html", []) ==
-             "Internal Server Error"
+    assert render_to_string(EventMishandlingWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
   end
 end
